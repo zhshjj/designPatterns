eb1265212849d2f518a8af181e595c7d238bbc3b
zhaoshuaijie <zhaoshuaijie@lightinthebox.com>
2022-07-04 15:32:42 +0800

    bridge.ts

---
+++ bridge.ts
@@ -0,0 +1,49 @@
+/**
+ * 桥接模式
+ */
+class Abstraction {
+	protected implementation: Implementation;
+
+	constructor(implementation: Implementation) {
+		this.implementation = implementation;
+	}
+
+	public operation(): string {
+		const result = this.implementation.operationImplementation();
+		return `Abstraction: Base operation with:\n${result}`;
+	}
+}
+
+class ExtendedAbstraction extends Abstraction {
+	public operation(): string {
+		const result = this.implementation.operationImplementation();
+		return `ExtendedAbstraction: Extended operation with:\n${result}`;
+	}
+}
+
+interface Implementation {
+	operationImplementation(): string;
+}
+
+class ConcreteImplementationA implements Implementation {
+	public operationImplementation(): string {
+		return "ConcreteImplementationA: Here's the result on the platform A.";
+	}
+}
+
+class ConcreteImplementationB implements Implementation {
+	public operationImplementation(): string {
+		return "ConcreteImplementationB: Here's the result on the platform B.";
+	}
+}
+
+function clientCode(abstract: Abstraction) {
+	console.log(abstract.operation());
+}
+
+const a = new Abstraction(new ConcreteImplementationA());
+clientCode(a);
+const b = new ExtendedAbstraction(new ConcreteImplementationB());
+clientCode(b);
+
+export default {};
